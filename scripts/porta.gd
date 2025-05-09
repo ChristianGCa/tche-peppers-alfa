fd531f48c948cdaa4d22f89a4f16e6841540dea9
ChristianGCa <candelonichristian@gmail.com>
2025-05-08 21:51:53 -0300

    Fazendo a porta funcional

--- scripts/porta.gd
+++ scripts/porta.gd
@@ -1,8 +1,13 @@
 extends Area2D
 
-@export var cena_destino: PackedScene
-@export var nome_ponto_entrada: String = "" # Nome do marcador no destino
+@export var nome_ponto_entrada: String = "porta_entrada_planetario" # Nome do marcador no destino
 @export var tecla_acao: String = "ui_accept"
+@export var cena_destino_path: String = ""
+var cena_destino: PackedScene
+
+func _ready():
+	if cena_destino_path != "":
+		cena_destino = load(cena_destino_path)
 
 var jogador_na_area = false
 
