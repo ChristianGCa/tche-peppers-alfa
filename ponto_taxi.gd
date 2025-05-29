60379de1001d09eda89f89f2f909d7dc81c627c5
Gabriel plagens schuinsekel <gabriel.schuinsekel@gmail.com>
2025-05-29 10:58:40 -0300

    Adicionando
    Ponto de taxi adicionado, parquímetro adicionado e carro adicionado na garagem da academia

---
+++ ponto_taxi.gd
@@ -0,0 +1,14 @@
+extends Node2D
+@export var transparent_opacity := 0.4 
+
+@onready var sprite = $Sprite2D  # Ajusta o nome aqui se teu sprite tiver outro nome
+
+func _on_area_2d_body_entered(body):
+	print("entrou")
+	print(body.name)
+	if body.name == "Player":
+		sprite.modulate.a = transparent_opacity
+
+func _on_area_2d_body_exited(body):
+	if body.name == "Player":
+		sprite.modulate.a = 1.0
