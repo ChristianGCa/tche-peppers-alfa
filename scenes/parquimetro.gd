ce5071622c76111736ad4686f6a367bdea905239
Gabriel plagens schuinsekel <gabriel.schuinsekel@gmail.com>
2025-05-30 09:25:59 -0300

    Atualização
    Atualização

--- scenes/parquimetro.gd
+++ scenes/parquimetro.gd
@@ -2,7 +2,10 @@ extends Node2D
 @export var transparent_opacity := 0.4 
 
 @onready var sprite = $Sprite2D  # Ajusta o nome aqui se teu sprite tiver outro nome
-
+func _process(delta):
+	if Input.is_action_just_pressed("ok"):
+		print("legal")
+		
 func _on_area_2d_body_entered(body):
 	print("entrou")
 	print(body.name)
@@ -12,3 +15,4 @@ func _on_area_2d_body_entered(body):
 func _on_area_2d_body_exited(body):
 	if body.name == "Player":
 		sprite.modulate.a = 1.0
+		
